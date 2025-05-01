e13b4804a1d618c3e7be0f06b0c78c12b8be69a9
KajitsuLight <maimusov67@gmail.com>
2025-05-01 10:54:34 +0300

    Add files via upload

---
+++ еу.js
@@ -0,0 +1,52 @@
+const gallery = document.getElementById('gallery');
+const nextButton = document.getElementById('nextButton');
+const prevButton = document.getElementById('prevButton');
+const nextButton1 = document.getElementById('nextButton1');
+const prevButton1 = document.getElementById('prevButton1');
+
+let scrollAmount = 0;
+const scrollStep = 260; // Шаг прокрутки
+
+const scrollToPosition = (amount) => {
+    gallery.scrollTo({
+        left: amount,
+        behavior: 'smooth' // Плавная прокрутка
+    });
+};
+
+const handleScroll = (direction) => {
+    scrollAmount += direction * scrollStep; // Увеличиваем или уменьшаем scrollAmount
+    if (scrollAmount < 0) scrollAmount = 0; // Не даем прокрутить в отрицательную сторону
+    scrollToPosition(scrollAmount);
+};
+
+nextButton1.addEventListener('click', () => handleScroll(1));
+nextButton.addEventListener('click', () => handleScroll(1));
+prevButton1.addEventListener('click', () => handleScroll(-1));
+prevButton.addEventListener('click', () => handleScroll(-1));
+
+const map = L.map('map').setView([54.766363, 32.061164], 15); 
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+function zoomIn() {
+    map.setZoom(map.getZoom() + 1);
+}
+
+
+function zoomOut() {
+    map.setZoom(map.getZoom() - 1);
+}
+
+map.on('mouseover', () => {
+    zoomIn(); // При наведении увеличиваем масштаб
+});
+
+map.on('mouseout', () => {
+    zoomOut(); // При уходе курсора уменьшаем масштаб
+});
